bff91820d6ff72fe94f6b54ba3cc7632c46bfe26
Neville Brailsford <neville_brailsford@hotmail.com>
2022-02-12 09:58:24 +0000

    Added ApplicationRegister class

---
+++ src/main/java/com/brailsoft/base/ApplicationRegister.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package com.brailsoft.base;
+
+/**
+ * @author nevil
+ *
+ */
+public class ApplicationRegister {
+	private static Application registeredApplication = null;
+
+	/**
+	 * Register the application class for this app.
+	 * 
+	 * @param application
+	 * @throws AssertionError if application is null, or an application has already
+	 *                        been registered.
+	 */
+	public synchronized static void registerApplication(Application application) {
+		assert (application != null);
+		assert (registeredApplication == null);
+		registeredApplication = application;
+	}
+
+	/**
+	 * Retrieve the application registered for this app
+	 * 
+	 * @return application
+	 * @throws AssertionError if no application has been registered.
+	 */
+	public synchronized static Application application() {
+		assert (registeredApplication != null);
+		return registeredApplication;
+	}
+
+	/**
+	 * Clear the registration
+	 */
+	public synchronized static void clear() {
+		registeredApplication = null;
+	}
+}
